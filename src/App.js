abfd70faaadad5d8e5e371b73f07be72644e586c
Thommzy <tomiwaobeisun@yahoo.com>
2019-06-17 18:28:42 +0100

    changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Elements/Header/Header";
+import Home from "./Elements/Home/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
